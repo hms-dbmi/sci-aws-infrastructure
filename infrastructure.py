a00d3c265f5c3a2030736222b45ba8d894f79f92
mtmcduffie <mcduffie.michael@gmail.com>
2016-12-21 13:55:45 -0500

    TC-11 - Updating Deployment.

--- infrastructure.py
+++ infrastructure.py
@@ -1,9 +1,13 @@
 import boto3
 import argparse
+import sys
+
+sys.path.insert(0, "/Users/mtmcduffie/src/devops/aws-python-utilities")
 
 from ecs import create_ecs_cluster, create_ecs_ec2, create_ecs_task
 from security_group import create_security_groups
 from utilities import read_settings_file
+from populate_vault import populate_vault_django_secret, populate_vault_auth0, populate_vault_auth0_full, populate_vault_registration_services
 
 parser = argparse.ArgumentParser()
 parser.add_argument("settings_file")
@@ -35,13 +39,39 @@ if steps["CREATE_CLUSTER"] == "True":
 if steps["CREATE_ECS_EC2"] == "True":
     create_ecs_ec2(stack_name, ecs_cluster_name, vpc, ec2, userdata_string, settings, ENVIRONMENT)
 
-if steps["CREATE_TASK"] == "True":
+if steps["CREATE_TASK_HYPATIO"] == "True":
     ecs_task_family = ecs_cluster_name
 
-    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT, "HYPATIO")
-    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT, "SCIAUTH")
-    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT, "SCIAUTHZ")
+    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT.lower(), "HYPATIO")
+
+if steps["CREATE_TASK_SCIAUTH"] == "True":
+    ecs_task_family = ecs_cluster_name
+
+    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT.lower(), "SCIAUTH")
+
+if steps["CREATE_TASK_SCIAUTHZ"] == "True":
+    ecs_task_family = ecs_cluster_name
+
+    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT.lower(), "SCIAUTHZ")
+
+if steps["CREATE_TASK_SCIREG"] == "True":
+    ecs_task_family = ecs_cluster_name
+
+    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT.lower(), "SCIREG")
+
+if steps["POPULATE_VAULT"] == "True":
+    # vault token-create -policy="sci-auth-dev-write" -policy="sci-authz-dev-write" -policy="sci-reg-dev-write" -policy="hypatio-dev-write" -ttl="300m" -format="json" | jq -r .auth.client_token
+    populate_vault_django_secret(settings, "sciauth/" + ENVIRONMENT.lower())
+    populate_vault_auth0_full(settings, "sciauth/" + ENVIRONMENT.lower())
+
+    populate_vault_django_secret(settings, "sciauthz/" + ENVIRONMENT.lower())
+
+    populate_vault_django_secret(settings, "scireg/" + ENVIRONMENT.lower())
+
+    populate_vault_registration_services(settings, "scireg/" + ENVIRONMENT.lower(), "SCIREG")
 
 
-
-
+if steps["POPULATE_VAULT_HYPATIO"] == "True":
+    populate_vault_django_secret(settings, "hypatio/" + ENVIRONMENT.lower())
+    populate_vault_auth0_full(settings, "hypatio/" + ENVIRONMENT.lower())
+    populate_vault_registration_services(settings, "hypatio/" + ENVIRONMENT.lower(), "HYPATIO")
--- utilities.py
+++
@@ -1,7 +0,0 @@
-def read_settings_file(filename):
-    d = {}
-    with open(filename) as f:
-        for line in f:
-            (key, val) = line.split("=")
-            d[key] = val.strip()
-    return d
