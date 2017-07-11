9fa7b84b211c81262d7aecd10979a18aba44888a
mtmcduffie <mcduffie.michael@gmail.com>
2017-07-11 11:47:51 -0400

    PPM-172 - Various improvements to deployment devops.

--- infrastructure.py
+++ infrastructure.py
@@ -46,11 +46,26 @@ userdata_string = "#!/bin/bash\necho ECS_CLUSTER=" + ecs_cluster_name + " >> /et
 MYSQL_USERNAME = "root"
 MYSQL_PORT = "3306"
 
+TASK_NAMES = ["SCIAUTH", "SCIREG", "SCIAUTHZ", "HYPATIO"]
+
 secret_list = {"SCIAUTH":[
                         {"secret_name": "mysql_username", "secret_value": "sciauth"},
                         {"secret_name": "mysql_port", "secret_value": MYSQL_PORT},
                         {"secret_name": "cookie_domain", "secret_value": settings["COOKIE_DOMAIN"]}
-                ,],
+                ],
+               "SCIAUTHZ":[
+                        {"secret_name": "mysql_username", "secret_value": "sciauthz"},
+                        {"secret_name": "mysql_port", "secret_value": MYSQL_PORT}
+                ],
+                "HYPATIO": [
+                    {"secret_name": "mysql_username", "secret_value": "hypatio"},
+                    {"secret_name": "mysql_port", "secret_value": MYSQL_PORT},
+                    {"secret_name": "permission_server_url", "secret_value": settings["PERMISSION_SERVER_URL"]},
+                    {"secret_name": "authentication_login_url", "secret_value": settings["ACCOUNT_SERVER_URL"]},
+                    {"secret_name": "register_user_url", "secret_value": settings["SCIREG_URL"]},
+                    {"secret_name": "authorization_server_url", "secret_value": settings["PERMISSION_SERVER_URL"]},
+                    {"secret_name": "cookie_domain", "secret_value": settings["COOKIE_DOMAIN"]},
+                ],
                "SCIREG":[
                         {"secret_name": "mysql_username", "secret_value": "scireg"},
                         {"secret_name": "mysql_port", "secret_value": MYSQL_PORT},
@@ -61,6 +76,7 @@ secret_list = {"SCIAUTH":[
                         {"secret_name": "email_host_user", "secret_value": settings["EMAIL_HOST_USER"]},
                         {"secret_name": "email_host_password", "secret_value": settings["EMAIL_HOST_PASSWORD"]},
                         {"secret_name": "email_port", "secret_value": settings["EMAIL_PORT"]},
+                        {"secret_name": "permission_server_url", "secret_value": settings["PERMISSION_SERVER_URL"]},
                       ]}
 
 if steps["CREATE_DB_SUBNETS"] == "True":
@@ -83,8 +99,6 @@ if steps["CREATE_ECS_EC2"] == "True":
 
 if steps["CREATE_SERVICE"] == "True":
 
-    TASK_NAMES = ["SCIAUTH", "SCIREG"]
-
     for task_name in TASK_NAMES:
         try:
             create_ecs_service(ecs_client, ecs_cluster_name, ecs_cluster_name + "-" + task_name)
@@ -94,8 +108,6 @@ if steps["CREATE_SERVICE"] == "True":
 
 if steps["CREATE_TASK_ROLE"] == "True":
 
-    TASK_NAMES = ["SCIAUTH", "SCIREG"]
-
     for task_name in TASK_NAMES:
 
         vault_path = settings["VAULT_PROJECT_NAME"] + "/" + task_name.lower() + "/" + ENVIRONMENT.upper()
@@ -104,8 +116,6 @@ if steps["CREATE_TASK_ROLE"] == "True":
         create_ecs_task_role(stack_name + "-" + task_name)
 
 if steps["CREATE_KEY_FOR_AWS_PS"] == "True":
-
-    TASK_NAMES = ["SCIAUTH", "SCIREG"]
 
     for task_name in TASK_NAMES:
 
@@ -123,8 +133,6 @@ if steps["CREATE_KEY_FOR_AWS_PS"] == "True":
 
 
 if steps["ADD_POLICY_TO_ECS_TASK_ROLE"] == "True":
-
-    TASK_NAMES = ["SCIAUTH", "SCIREG"]
 
     for task_name in TASK_NAMES:
         add_policy_to_ecs_task_role(stack_name + "-" + task_name, settings)
@@ -147,45 +155,18 @@ if steps["CREATE_TASK_SCIAUTHZ"] == "True":
 if steps["CREATE_TASK_SCIREG"] == "True":
     ecs_task_family = ecs_cluster_name
 
-    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT.lower(), "SCIREG")
+    create_ecs_task(ecs_client, ecs_task_family, ecs_cluster_name, settings, ENVIRONMENT.lower(), "SCIREG", "arn:aws:iam::685606823951:role/SCI-DEV-SCIREG-TASK-ROLE")
 
 if steps["CREATE_RDS"] == "True":
     create_db(stack_name, vpc, rds_client, settings, ENVIRONMENT, "SCI")
 
 if steps["CREATE_DBS"] == "True":
-    create_database_for_task(settings, "SCI", "HYPATIO", ENVIRONMENT)
-    create_database_for_task(settings, "SCI", "SCIAUTH", ENVIRONMENT)
-    create_database_for_task(settings, "SCI", "SCIAUTHZ", ENVIRONMENT)
-    create_database_for_task(settings, "SCI", "SCIREG", ENVIRONMENT)
 
-if steps["POPULATE_VAULT"] == "True":
-    # vault token-create -policy="sci-dev-write" -policy="sci-auth-dev-write" -policy="sci-authz-dev-write" -policy="scireg-dev-write" -policy="hypatio-dev-write" -ttl="300m" -format="json" | jq -r .auth.client_token
-    # vault token-create -policy="sci-prod-write" -policy="sci-auth-prod-write" -policy="sci-authz-prod-write" -policy="scireg-prod-write" -policy="hypatio-prod-write" -ttl="300m" -format="json" | jq -r .auth.client_token
-
-    vault_path_sciauthz = settings["VAULT_PROJECT_NAME"] + "/sciauthz/" + ENVIRONMENT.lower()
-    vault_path_scireg = settings["VAULT_PROJECT_NAME"] + "/scireg/" + ENVIRONMENT.lower()
-
-    populate_vault_django_secret(settings, ENVIRONMENT.lower(), "sciauthz")
-    populate_vault_django_secret(settings, ENVIRONMENT.lower(), "scireg")
-
-    populate_vault_auth0_full(settings, ENVIRONMENT.lower(), "sciauthz")
-    populate_vault_auth0_full(settings, ENVIRONMENT.lower(), "scireg")
-
-    populate_vault_registration_services(settings, "scireg/" + ENVIRONMENT.lower(), "SCIREG")
-
-    secret_to_vault(settings, vault_path_sciauthz + "/mysql_username", "sciauthz")
-    secret_to_vault(settings, vault_path_sciauthz + "/mysql_port", MYSQL_PORT)
-
-    secret_to_vault(settings, vault_path_scireg + "/ssl_key", read_key_file(settings["SSL_KEY_FILE_SCIREG"]).decode("utf-8"))
-    secret_to_vault(settings, vault_path_scireg + "/ssl_cert_chain", read_key_file(settings["SSL_CERT_CHAIN_FILE_SCIREG"]).decode("utf-8"))
-
-    secret_to_vault(settings, vault_path_sciauthz + "/ssl_key", read_key_file(settings["SSL_KEY_FILE_SCIAUTHZ"]).decode("utf-8"))
-    secret_to_vault(settings, vault_path_sciauthz + "/ssl_cert_chain", read_key_file(settings["SSL_CERT_CHAIN_FILE_SCIAUTHZ"]).decode("utf-8"))
+    for task_name in TASK_NAMES:
+        create_database_for_task(settings, "SCI", task_name, ENVIRONMENT)
 
 
 if steps["POPULATE_AWS_PARAMETER_STORE"] == "True":
-
-    TASK_NAMES = ["SCIAUTH", "SCIREG"]
 
     for task_name in TASK_NAMES:
 
@@ -206,7 +187,7 @@ if steps["POPULATE_AWS_PARAMETER_STORE"] == "True":
         populate_ps_django_secret(settings, ssm_client, ENVIRONMENT.upper(), task_name.lower(), key_name, dry_run)
         populate_ps_auth0(settings, ssm_client, ENVIRONMENT.upper(), task_name.lower(), key_name, dry_run)
 
-        if task_name == "SCIAUTH":
+        if task_name == "SCIAUTH" or task_name == "HYPATIO":
             populate_ps_auth0_full(settings, ssm_client, ENVIRONMENT.upper(), task_name.lower(), key_name, dry_run)
 
         # I know, I know. The Cert is too long for one SecureString. Split it to multiple secrets.
@@ -221,25 +202,7 @@ if steps["POPULATE_AWS_PARAMETER_STORE"] == "True":
         secret_to_ps(ssm_client, vault_path + "ssl_cert_chain3", ssl_cert[2], key_name, dry_run)
 
         secret_to_ps(ssm_client, vault_path + "mysql_host", settings["DB_HOST"], key_name, dry_run)
-        secret_to_ps(ssm_client, vault_path + "mysql_pw", settings["DB_PW"], key_name, dry_run)
-
-if steps["POPULATE_VAULT_HYPATIO"] == "True":
-    vault_path_hypatio = settings["VAULT_PROJECT_NAME"] + "/hypatio/" + ENVIRONMENT.lower()
-
-    populate_vault_django_secret(settings, ENVIRONMENT.lower(), "hypatio")
-    populate_vault_auth0_full(settings, ENVIRONMENT.lower(), "hypatio")
-    populate_vault_registration_services(settings, "hypatio/" + ENVIRONMENT.lower(), "HYPATIO")
-
-    secret_to_vault(settings, vault_path_hypatio + "/authentication_login_url", settings["ACCOUNT_SERVER_URL"])
-    secret_to_vault(settings, vault_path_hypatio + "/register_user_url", settings["SCIREG_URL"])
-    secret_to_vault(settings, vault_path_hypatio + "/authorization_server_url", settings["PERMISSION_SERVER_URL"])
-
-    secret_to_vault(settings, vault_path_hypatio + "/ssl_key", read_key_file(settings["SSL_KEY_FILE_HYPATIO"]).decode("utf-8"))
-    secret_to_vault(settings, vault_path_hypatio + "/ssl_cert_chain", read_key_file(settings["SSL_CERT_CHAIN_FILE_HYPATIO"]).decode("utf-8"))
-
-    secret_to_vault(settings, vault_path_hypatio + "/cookie_domain", settings["COOKIE_DOMAIN"])
-
-    secret_to_vault(settings, vault_path_hypatio + "/authentication_login_url", settings["ACCOUNT_SERVER_URL"])
+        secret_to_ps(ssm_client, vault_path + "mysql_pw", settings["DB_PW_" + task_name], key_name, dry_run)
 
 if steps["ADD_SG_IO"] == "True":
 
@@ -258,15 +221,11 @@ if steps["ADD_SG_IO"] == "True":
 
 if steps["CREATE_CODEBUILD"] == "True":
 
-    TASK_NAMES = ["SCIAUTH", "SCIREG"]
-
     for task_name in TASK_NAMES:
 
         create_codebuild(stack_name + "-" + task_name, task_name.lower(), ENVIRONMENT.lower(), settings)
 
 if steps["CREATE_CODEPIPELINE"] == "True":
-
-    TASK_NAMES = ["SCIAUTH", "SCIREG"]
 
     for task_name in TASK_NAMES:
 
